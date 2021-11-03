1dea35bfbfcd454fcb9902c179e6b1c1812e4b89
Guillermo Rivas <guillerivasunion@gmail.com>
2021-11-02 23:55:38 -0300

    tercer entregable

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let mensajeBienvenida = "¡Bienvenido/a " + nombreUsuario + "!";
 alert(mensajeBienvenida) */
 
-const usuario = prompt("Ingrese su nombre de usuario");
+/*const usuario = prompt("Ingrese su nombre de usuario");
 const contraseña = prompt("ingrese su contraseña");
 
 const usuarioAdmin = (usuario == "marcoMeAprobas" && contraseña == "PorFavor")
@@ -16,4 +16,23 @@ else if (usuarioRegular) {
 }
 else {
     alert("Datos Incorrectos")
+} */
+
+const numero = Number(prompt("Que tabla querés saber"));
+
+for (let i = 0; i <= 10; i++) {
+    console.log( `${numero}x${i} = ${numero * i}`);
 }
+
+let contraseña;
+let intentos = 5;
+
+do{
+    contraseña = prompt("Ingrese su contraseña");
+    if (contraseña !== JuanPerez || contraseña !== RobertoCarlos || contraseña !== EzequielGarcia){
+        alert("Contraseña incorrecta");
+        intentos--;
+    }
+
+}while (contraseña == EzequielGarcia || contraseña == RobertoCarlos || contraseña == JuanPerez);
+alert("Contrasela Correcta")
